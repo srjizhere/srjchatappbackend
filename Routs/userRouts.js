fd9d5242530c7ca207cf6d659d4e2b7e10f54844
srjizhere <bhiapatil@gmail.com>
2023-02-19 10:42:49 +0530

    deplymentonly

---
+++ Routs/userRouts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {registerUser,authuser, allUsers} = require('../controllers/userControllers');
+const { protect} = require('../middelware/authmiddelware')
+const router = express.Router()
+router.route('/').post(registerUser).get(protect,allUsers);
+router.post('/login',authuser)
+
+
+
+
+module.exports = router;
