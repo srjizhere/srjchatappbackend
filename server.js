94788b3c2c4f5e3650f16c3e0ccb343ee4cad8d7
srjizhere <bhiapatil@gmail.com>
2023-06-20 01:40:18 +0530

    port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errHandler);
 
-const PORT = process.env.port;
+const PORT = process.env.port || 8080;
 
 const server = app.listen(
   PORT,
