23ebc36dd03e787e10f7474433cafa0e2df5d45e
srjizhere <bhiapatil@gmail.com>
2023-06-20 01:05:44 +0530

    cors

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   },
 });
